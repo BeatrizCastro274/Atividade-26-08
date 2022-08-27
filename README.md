904baedf4408328e596b2464b8f0ef94cd93c423
BeatrizCastro274 <biawolf4@hotmail.com>
2022-08-26 21:41:14 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# typescript-zq7qnd
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/typescript-zq7qnd)
---
+++ index.ts
@@ -0,0 +1,9 @@
+// Import stylesheets
+import './style.css';
+import { Person } from './person.ts';
+
+let title: string = 'Aula 26/08';
+
+// Write TypeScript code!
+const appDiv: HTMLElement = document.getElementById('app');
+appDiv.innerHTML = `<h1>TypeScript Starter</h1>`;
