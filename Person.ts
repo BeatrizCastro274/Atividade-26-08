019b248a57b53bc8279a3f79b6b274ac6b093208
BeatrizCastro274 <biawolf4@hotmail.com>
2022-08-26 22:03:30 -0300

    Modelo

--- Person.ts
+++ Person.ts
@@ -1,18 +1,9 @@
 export class Person {
-  constructor() {
-    function fullName(pessoa: { name: string; lastname: string }): string {
-      return pessoa.name + ' ' + pessoa.lastname;
-    }
-
-    const appDivFunc: HTMLElement = document.getElementById('app-Func');
-    appDivFunc.innerHTML = `
-  <h1>Functions</h1>
-  <h2> Soma de 2 e 3: ${sum(2, 3)}</h2>
-  <h2> Soma de 11 e 329: ${sum(11, 329)}</h2>
-  <h2> Meu nome completo: ${fullName({
-    lastname: 'Araujo',
-    name: 'Beatriz C.',
-  })}</h2>
-  `;
+  greeting: string;
+  constructor(message: string) {
+    this.greeting = message;
+  }
+  getGreeting() {
+    return 'Olá, ' + this.greeting;
   }
 }
--- index.ts
+++ index.ts
@@ -1,9 +1,28 @@
 // Import stylesheets
 import './style.css';
-import { Person } from './person.ts';
+import { Person } from './Person.ts';
 
 let title: string = 'Aula 26/08';
+let paragraph: string = 'Blablabla...';
+
+let counter = 0;
+
+let greeter = new Person('Impiedoso');
+
+let IntervalId = setInterval(() => {
+  counter = counter + 1;
+  const appP: HTMLElement = document.getElementById('app.p.counter');
+  appP.innerHTML = 'Contador:' + counter;
+}, 1000);
+
+const btn = document.getElementById('app.btn');
+btn?.addEventListener('click', alertMethod);
+function alertMethod(this: HTMLElement, ev: Event) {
+  alert('Alertando com TypeScript');
+}
 
 // Write TypeScript code!
-const appDiv: HTMLElement = document.getElementById('app');
-appDiv.innerHTML = `<h1>TypeScript Starter</h1>`;
+const appH1: HTMLElement = document.getElementById('app.h1');
+appH1.innerHTML = title;
+const appP: HTMLElement = document.getElementById('app.p');
+appP.innerHTML = greeter.getGreeting();
